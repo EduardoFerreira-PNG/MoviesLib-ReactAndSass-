977eb8440ea7e27082816ee3cb11e9feda1825a1
Eduardo <eduardonaves02@outlook.com>
2022-11-14 18:58:10 -0300

    atualização projeto movie

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,32 @@
-import { Link } from "react-router-dom"
+import{useState} from 'react'
+import { Link, useNavigate } from "react-router-dom"
 import{BiCameraMovie, BiSearchAlt2} from "react-icons/bi"
 
 import "../styles/navbar.sass"
 
 const Navbar = () => {
+  const [search, setSearch] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(!search) return
+
+    navigate(`/search?q=${search}`)
+    setSearch("");
+  }
+
+
+
   return (
     <nav id="navbar">
         <h2 className="logo">
           <Link to="/"><BiCameraMovie/>MoviesLib</Link>
         </h2>
-        <form>
-            <input type="text" placeholder="Busque um filme"/>
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Busque um filme" onChange={(e) => setSearch(e.target.value)}
+            value = {search}/>
             <button type="submit"><BiSearchAlt2 /></button>
 
         </form>
