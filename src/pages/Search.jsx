9b105db1320a75cf8d86aaaf1efa46919f17a8b6
Eduardo <eduardonaves02@outlook.com>
2022-11-16 16:34:21 -0300

    finalização projeto movieslib

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -13,15 +13,30 @@ const Search = () => {
 
   const [searchParams] = useSearchParams();
 
-  const [topMovies,setTopMovies] = useState([])
+  const [movies,setMovies] = useState([])
   const query = searchParams.get("q")
+
+  const getSearchedMovies = async (url) => {
+    const rest = await fetch(url)
+    const data = await rest.json();
+
+    setMovies(data.results)
+  };
+
+  useEffect(() => {
+
+    const searchWithQueryURL = `${searchURL}?${apiKey}&query=${query}`
+
+    getSearchedMovies(searchWithQueryURL)
+
+  },[query])
 
   return (
     <div className="container">
         <h2 className="title">Resultados para: <span className="query-text">{query}</span></h2>
         <div className="movies-container">
-          {topMovies.length === 0 && <p>Carregando...</p> }
-        {topMovies.length > 0 && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie}/> )}
+          {movies.length === 0 && <p>Carregando...</p> }
+        {movies.length > 0 && movies.map((movie) => <MovieCard key={movie.id} movie={movie}/> )}
         </div>
     </div>
   )
